cc24f92754ffe71a2a44d1151171dd4101ee0227
Pramod Kumar <pramod9ks@gmail.com>
2020-12-13 15:22:40 +0530

    initial commit

---
+++ src/main/java/com/vdart/assetmanagement/controller/AssetManagementController.java
@@ -0,0 +1,10 @@
+package com.vdart.assetmanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/api")
+public class AssetManagementController {
+
+}
